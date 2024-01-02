5c667e3cd38018197eb8f9288fa66fe741c67c81
yaroslavn <yaroslav.n@open-tag.com>
2024-01-02 21:31:57 +0200

    Authorization: created authorization for admin panel.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes([
+    'reset' => false,
+    'confirm' => false,
+    'verify' => false,
+]);
 
+Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('get-logout');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
 Route::get('/categories', 'App\Http\Controllers\MainController@categories')->name('categories');
 Route::get('/basket', 'App\Http\Controllers\BasketController@basket')->name('basket');
@@ -23,4 +30,8 @@ Route::post('/basket/remove/{id}', 'App\Http\Controllers\BasketController@remove
 
 
 Route::get('/categories/{category}', 'App\Http\Controllers\MainController@category')->name('category');
-Route::get('/{category}/{product?}', 'App\Http\Controllers\MainController@show_product')->name('show_product');
+//Route::get('/{category}/{product?}', 'App\Http\Controllers\MainController@show_product')->name('show_product');
+
+Auth::routes();
+
+
